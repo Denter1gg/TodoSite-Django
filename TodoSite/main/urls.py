f2215542da0bd102a8ec7d55177c65b5e418b1da
Denter1gg <dentop1gg.yt@gmail.com>
2023-12-04 19:50:19 +0100

    Commit 2 - Create Task Page

--- TodoSite/main/urls.py
+++ TodoSite/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 
     path('', views.tables),
+    path('create_task/', views.create_task, name="create_task"),
 
 ]
--- TodoSite/main/views.py
+++ TodoSite/main/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
+
+ASSETS_ROOT = "/static/assets"
+
 def tables(request):
-    return render(request, 'main/tables.html', )
+
+    context = {
+        'ASSETS_ROOT' : ASSETS_ROOT,
+
+    }
+    return render(request, 'main/tables.html', context)
+
+def create_task(request):
+
+    context = {
+        'ASSETS_ROOT' : ASSETS_ROOT,
+
+    }
+    return render(request, 'main/user.html', context)
